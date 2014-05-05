e0f4c0beadcb1864172ac6ec196d8ca9465ea927
Brian Auton <brianauton@gmail.com>
2014-05-05 13:22:14 -0400

    Delay config file loading to runtime, to fix Rails init order issue

--- lib/gm_plugin/key.rb
+++ lib/gm_plugin/key.rb
@@ -8,28 +8,34 @@ module Ym4r
     
     #Class fo the manipulation of the API key
     class ApiKey
-      #Read the API key config for the current ENV
-      unless File.exist?(Rails.root.join("config","gmaps_api_key.yml"))
-        raise GMapsAPIKeyConfigFileNotFoundException.new("File config/gmaps_api_key.yml not found")
-      else
-        env = ENV['RAILS_ENV'] || Rails.env
-        GMAPS_API_KEY = YAML.load_file(Rails.root.join("config","gmaps_api_key.yml"))[env]
+
+      def self.load_key
+        unless File.exist?(Rails.root.join("config","gmaps_api_key.yml"))
+          raise GMapsAPIKeyConfigFileNotFoundException.new("File config/gmaps_api_key.yml not found")
+        else
+          env = ENV['RAILS_ENV'] || Rails.env
+          gmaps_api_key = YAML.load_file(Rails.root.join("config","gmaps_api_key.yml"))[env]
+        end
       end
-      
+
+      def self.gmaps_api_key
+        @gmaps_api_key = load_key
+      end
+
       def self.get(options = {})
         if options.has_key?(:key)
           options[:key]
-        elsif GMAPS_API_KEY.is_a?(Hash)
+        elsif gmaps_api_key.is_a?(Hash)
           #For this environment, multiple hosts are possible.
           #:host must have been passed as option
           if options.has_key?(:host)
-            GMAPS_API_KEY[options[:host]]
+            gmaps_api_key[options[:host]]
           else
-            raise AmbiguousGMapsAPIKeyException.new(GMAPS_API_KEY.keys.join(","))
+            raise AmbiguousGMapsAPIKeyException.new(gmaps_api_key.keys.join(","))
           end
         else
           #Only one possible key: take it and ignore the :host option if it is there
-          GMAPS_API_KEY
+          gmaps_api_key
         end
       end
     end
